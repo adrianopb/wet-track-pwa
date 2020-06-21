2edc8f5450ffc16156d00f37e203387219a9b27a
adrianopb <adrianopbraga1@gmail.com>
2020-06-21 20:31:52 -0300

    Adição dos js e css e service-worker;

---
+++ js/app.js
@@ -0,0 +1,142 @@
+let deferredInstallPrompt = null;
+const botaoInstalar = document.getElementById('btInstalar');
+
+let initialiseUI = function(){
+    botaoInstalar.removeAttribute('hidden');
+    botaoInstalar.addEventListener('click', function(){
+
+        deferredInstallPrompt.prompt();
+
+        deferredInstallPrompt.userChoice.then((choice) => {
+
+            if(choice.outcome === 'accepted'){
+
+                console.log("Usuário aceitou a instalação");
+
+            }else{
+
+                console.log("Usuário não aceitou a instalação");
+
+            }
+
+        });
+
+    });
+
+}
+
+window.addEventListener('beforeinstallprompt', gravarEvento);
+
+function gravarEvento(evt){
+    console.log("teste");
+    deferredInstallPrompt = evt;
+}
+
+
+//Carregar conteúdo do servidor
+var ajax = new XMLHttpRequest();
+
+ajax.open("GET", "./dados.json", true);
+
+ajax.send();
+
+ajax.onreadystatechange = function(){
+
+    if(ajax.readyState == 4 && ajax.status == 200){
+        var data = ajax.responseText;
+        var data_json = JSON.parse(data);
+        var conteudo = document.getElementById('park_content');
+
+        if(data_json.home_cards.length == 0){
+
+            conteudo.innerHTML = '<div class="row"><div class="col-12"><div class="alert alert-danger" role="alert">Nenhum parque cadastrado!</div></div></div>';
+
+        } 
+        else {
+
+            var html_conteudo = "";
+
+            for(var i=0; i < data_json.home_cards.length; i++){
+                html_conteudo+= park_card(data_json.home_cards[i].alt, data_json.home_cards[i].title, data_json.home_cards[i].href, data_json.home_cards[i].img, data_json.home_cards[i].description,data_json.home_cards[i].grade);
+            }
+
+            //Gravar a criação dos elementos
+            conteudo.innerHTML = html_conteudo;
+            cache_cards(data_json);
+        }
+    }
+}
+
+//Template do Card de Parques
+let park_card = function(alt, title, href, img, description, grade){
+
+    let grade_class_color = '';
+
+    if(grade < 5) {
+        grade_class_color = 'bad';
+    } 
+
+    else if(grade == 5) {
+        grade_class_color = 'medium';
+    } 
+
+    else if(grade > 5 && grade < 8) {
+        grade_class_color = 'good';
+    } 
+
+    else if(grade >= 8) {
+        grade_class_color = 'great';
+    } 
+
+    return  '<div class="col-12 mt-4 park-list">' +
+                '<a href="'+ href + '">' +
+                    '<div class="card">' +
+                        '<img src="'+ img +'" class="card-img-top" alt="'+ alt +'">' +
+                        '<div class="card-body">' +
+                            '<p class="card-title">'+ title +'</p>' +
+                            '<span class="card-text">'+ description +'</span>' +
+                            '<span class="float-right"><span class="card-grade '+ grade_class_color +'">'+ grade +'</span>/10</span>' +
+                        '</div>' +
+                    '</div>' +
+                '</a>' +
+            '</div>';
+
+}
+
+//Cache conteúdo dinâmico
+var cache_cards = function(data_json){
+
+    if('caches' in window){
+
+        caches.delete('wet-track-app-conteudo').then(function(){
+
+            console.log('Deletando cache de conteúdo antigo');
+
+            if(data_json.length > 0){
+
+                var files = ['dados.json'];
+
+                //Entrando na categoria
+                for(var i = 0; i < data_json.length; i++){
+
+                    //Entrando no item
+                    for(var j = 0; j < data_json[i].itens.length; j++){
+                        if(files.indexOf(data_json[i].itens[j].imagem) == -1){
+                            files.push(data_json[i].itens[j].imagem);
+                        }
+                    }
+                }
+
+                caches.open('wet-track-app-conteudo').then(function (cache){
+
+                    cache.addAll(files).then(function(){
+                        console.log("Arquivos de conteúdo cacheados!");
+                    });
+
+                });
+            }
+
+        });
+
+    }
+}
---
+++ service-worker.js
@@ -0,0 +1,75 @@
+'use strict';
+
+const CACHE_NAME = 'wet-track-v1';
+
+const FILES_TO_CACHE = [
+    'images/icons/favicon.ico',
+    'images/logo.png',
+    'images/water-bg.jpeg',
+    'css/styles.css',
+    'bootstrap/css/bootstrap.min.css',
+    'bootstrap/css/bootstrap.min.css.map',
+    'bootstrap/js/bootstrap.min.js',
+    'bootstrap/js/bootstrap.min.js.map',
+    'js/jquery-3.5.1.min.js',
+    'js/popper.min.js',
+    'js/popper.min.js.map',
+    'offline.html',
+];
+
+//Instala o service worker no browser
+self.addEventListener('install', (evt) => {
+
+    console.log('[ServiceWorker] Instalando...');
+
+    evt.waitUntil(
+
+        caches.open(CACHE_NAME).then((cache) => {
+
+            console.log('[ServiceWorker] Fazendo cache dos arquivos da aplicação');
+            return cache.addAll(FILES_TO_CACHE);
+        })
+
+    );
+    self.skipWaiting();
+});
+
+//Gera o CACHE dos arquivos estáticos
+self.addEventListener('activate', (evt) => {
+
+    console.log('[ServiceWorker] Ativando...');
+
+    evt.waitUntil(
+
+        caches.keys().then((keylist) => {
+
+            return Promise.all(keylist.map((key) => {
+
+                //console.log('[ServiceWorker] Removendo cache antigo...');
+                if(key !== CACHE_NAME){
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
+    self.clients.claim();
+});
+
+//Responder a versão offline do app
+self.addEventListener('fetch', (evt) => {
+    console.log('[ServiceWorker] Recebendo', evt.request.url);
+
+    if (evt.request.mode !== 'navigate') {
+        return;
+    }
+    evt.respondWith(
+        fetch(evt.request)
+            .catch(() => {
+                return caches.open(CACHE_NAME)
+                    .then((cache) => {
+                        return cache.match('/offline.html');
+                    });
+            })
+    );
+
+});
